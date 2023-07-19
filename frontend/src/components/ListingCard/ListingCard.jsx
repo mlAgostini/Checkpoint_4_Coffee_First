d0ceadedf7fe9b49ae4fd63f23491d75e311dcd6
mlAgostini <valmont.vakarian@gmail.com>
2023-07-19 16:46:21 +0200

    adding cards to display the list

---
+++ frontend/src/components/ListingCard/ListingCard.jsx
@@ -0,0 +1,9 @@
+function ListingCard() {
+  return (
+    <di>
+      <h3>Je suis une carte avec une liste de noms</h3>
+    </di>
+  );
+}
+
+export default ListingCard;
--- frontend/src/pages/ListingView/ListingView.jsx
+++ frontend/src/pages/ListingView/ListingView.jsx
@@ -3,15 +3,17 @@ import ButtonHome from "../../components/ButtonHome/ButtonHome";
 import ButtonRandom from "../../components/ButtonRandom/ButtonRandom";
 import ButtonRegenerate from "../../components/ButtonRegenerate/ButtonRegenerate";
 import { ButtonContext } from "../../../contexts/ButtonContext";
+import ListingCard from "../../components/ListingCard/ListingCard";
 
 function ListingView() {
   const { isButtonClicked } = useContext(ButtonContext);
 
   return (
-    <div>
+    <div className="card-container">
       <h1>Je suis la page du tableau</h1>
+      <ListingCard />
+      <ListingCard />
       {isButtonClicked ? <ButtonRegenerate /> : <ButtonRandom />}
-
       <ButtonHome />
     </div>
   );
