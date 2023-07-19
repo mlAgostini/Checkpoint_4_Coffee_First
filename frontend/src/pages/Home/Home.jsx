93b24eccd464c5c6125579c9295977f4f543a143
mlAgostini <valmont.vakarian@gmail.com>
2023-07-19 16:01:30 +0200

    finish button state and modifications on click

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,6 +1,14 @@
+import { useContext, useEffect } from "react";
 import ButtonRandom from "../../components/ButtonRandom/ButtonRandom";
+import { ButtonContext } from "../../../contexts/ButtonContext";
 
 export default function Home() {
+  const { setIsButtonClicked } = useContext(ButtonContext);
+
+  useEffect(() => {
+    setIsButtonClicked(false);
+  }, []);
+
   return (
     <header className="App-header">
       <h1>Tired of fighting for the coffee machine ?</h1>
